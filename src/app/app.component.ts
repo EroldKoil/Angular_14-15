b7cb2e693ce3d21968d549bc394d2248b76e7a78
Mikalai Yakubau <mikalai.yakubau@ukg.com>
2023-05-17 01:24:40 +0300

    Task-2: fix useless services providers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 
-import { ProductsService } from '../core/productListComponent/productService';
-import { CartService } from '../core/productListComponent/cartService';
-import { Product } from '../core/productListComponent/products.interface';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
-  providers:  [ ProductsService, CartService ]
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   @ViewChild('appTitle') appTitle!: ElementRef<HTMLInputElement>;
